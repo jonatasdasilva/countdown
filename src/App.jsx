5a751166e970e70617fc64815fa0561cce2a98ad
Jônatas da Silva <jonatasdasilvasantos@outlook.com>
2025-04-02 21:28:30 -0300

    🎉 Commit inicial
    inicializa o projeto.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [days, setDays] = useState(0)
+  const [hours, setHours] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [seconds, setSeconds] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
